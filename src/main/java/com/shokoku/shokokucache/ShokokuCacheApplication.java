291ce13d0d0678019ad7051272b0e458c3f944e0
hyeonggyu-kim <shokoku0612@gmail.com>
2025-12-03 16:07:38 +0900

    feat: init project

---
+++ src/main/java/com/shokoku/shokokucache/ShokokuCacheApplication.java
@@ -0,0 +1,13 @@
+package com.shokoku.shokokucache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShokokuCacheApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShokokuCacheApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shokoku/shokokucache/ShokokuCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shokoku.shokokucache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShokokuCacheApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
