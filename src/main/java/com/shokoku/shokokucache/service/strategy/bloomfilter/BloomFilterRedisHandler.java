843c22f479bffc6e1d04f93515169c2b64f90c8c
hyeonggyu-kim <shokoku0612@gmail.com>
2025-12-03 21:51:22 +0900

    feat: bloom filter redis 핸들러 구현

---
+++ src/main/java/com/shokoku/shokokucache/service/strategy/bloomfilter/BloomFilterRedisHandler.java
@@ -0,0 +1,65 @@
+package com.shokoku.shokokucache.service.strategy.bloomfilter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class BloomFilterRedisHandler {
+  private final StringRedisTemplate redisTemplate;
+
+  /**
+   * 최초에 큰 메모리를 할당하려면 블로킹이 생길 수 있으며, 싱글 스레드 특성 상 다른 연산에 지연이 생길 수도 있다.
+   * BloomFilter 활성화 전에 내부 관리 도구에서 점차 메모리를 늘려가는 전략으로 미리 필요한 만큼 할당해둘 수 있다.
+   */
+  public void init(BloomFilter bloomFilter) {
+    String key = genKey(bloomFilter);
+    for (long offset = 0; offset < bloomFilter.getBitSize(); offset+= 8L * 1024 * 1024 * 8 /* 8MB */) {
+      redisTemplate.opsForValue().setBit(key, offset, false);
+    }
+  }
+
+  public void add(BloomFilter bloomFilter, String value) {
+    redisTemplate.executePipelined((RedisCallback<?>) action -> {
+      StringRedisConnection conn = (StringRedisConnection) action;
+      String key = genKey(bloomFilter);
+      List<Long> hashedIndexes = bloomFilter.hash(value);
+      for (Long hashedIndex : hashedIndexes) {
+        conn.setBit(key, hashedIndex, true);
+      }
+      return null;
+    });
+  }
+
+  public boolean mightContain(BloomFilter bloomFilter, String value) {
+    return redisTemplate.executePipelined((RedisCallback<?>) action -> {
+      StringRedisConnection conn = (StringRedisConnection) action;
+      String key = genKey(bloomFilter);
+      List<Long> hashedIndexes = bloomFilter.hash(value);
+      for (Long hashedIndex : hashedIndexes) {
+        conn.getBit(key, hashedIndex);
+      }
+      return null;
+    })
+            .stream()
+            .map(Boolean.class::cast)
+            .allMatch(Boolean.TRUE::equals);
+  }
+
+  public void delete(BloomFilter bloomFilter) {
+    redisTemplate.delete(genKey(bloomFilter));
+  }
+
+  private String genKey(BloomFilter bloomFilter) {
+    return genKey(bloomFilter.getId());
+  }
+
+  private String genKey(String id) {
+    return "bloom-filter:%s".formatted(id);
+  }
+}
---
+++ src/test/java/com/shokoku/shokokucache/service/strategy/bloomfilter/BloomFilterRedisHandlerTest.java
@@ -0,0 +1,98 @@
+package com.shokoku.shokokucache.service.strategy.bloomfilter;
+
+import com.shokoku.shokokucache.RedisTestContainerSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class BloomFilterRedisHandlerTest extends RedisTestContainerSupport {
+
+  @Autowired
+  BloomFilterRedisHandler bloomFilterRedisHandler;
+
+  @Test
+  void add() {
+    BloomFilter bloomFilter = BloomFilter.create("testId", 1000, 0.01);
+
+    bloomFilterRedisHandler.add(bloomFilter, "value");
+
+    List<Long> hashedIndexes = bloomFilter.hash("value");
+    for (long offset = 0; offset < hashedIndexes.size(); offset++) {
+      Boolean result = redisTemplate.opsForValue().getBit("bloom-filter:" + bloomFilter.getId(), offset);
+      assertThat(result).isEqualTo(hashedIndexes.contains(offset));
+    }
+  }
+
+  @Test
+  void delete() {
+    BloomFilter bloomFilter = BloomFilter.create("testId", 1000, 0.01);
+
+    bloomFilterRedisHandler.add(bloomFilter, "value");
+    bloomFilterRedisHandler.delete(bloomFilter);
+
+    for (long offset = 0; offset < bloomFilter.getBitSize(); offset++) {
+      Boolean result = redisTemplate.opsForValue().getBit("bloom-filter:" + bloomFilter.getId(), offset);
+      assertThat(result).isFalse();
+    }
+  }
+
+  @Test
+  void mightContain() {
+    BloomFilter bloomFilter = BloomFilter.create("testId", 1000, 0.01);
+
+    List<String> values = IntStream.range(0, 1000).mapToObj(idx -> "value" + idx).toList();
+    for (String value : values) {
+      bloomFilterRedisHandler.add(bloomFilter, value);
+    }
+
+    for (String value : values) {
+      assertThat(bloomFilterRedisHandler.mightContain(bloomFilter, value)).isTrue();
+    }
+
+    // 10000번 중에 93 번 오차 발생 0.01
+    for(int i = 0; i < 10000; i++) {
+      String value = "notAddedValue" + i;
+      boolean result = bloomFilterRedisHandler.mightContain(bloomFilter, value);
+      if (result) {
+        System.out.println("value = " + value);
+      }
+    }
+  }
+
+  @Test
+  void printExecutionTime_addToLargeBloomFilter() {
+    BloomFilter bloomFilter = BloomFilter.create("testId", 400_000_000, 0.01);
+    List<Long> hashedIndexes = bloomFilter.hash("value");
+    System.out.println("bloomFilter.getBitSize() = " + bloomFilter.getBitSize());
+    System.out.println("hashedIndexes = " + hashedIndexes);
+
+    long start = System.nanoTime();
+    bloomFilterRedisHandler.add(bloomFilter, "value");
+    long millis = Duration.ofNanos(System.nanoTime() - start).toMillis();
+    System.out.println("millis = " + millis);
+  }
+
+  @Test
+  void printExecutionTime_addToLargeBloomFilterAfterInit() {
+    BloomFilter bloomFilter = BloomFilter.create("testId", 400_000_000, 0.01);
+    List<Long> hashedIndexes = bloomFilter.hash("value");
+    System.out.println("bloomFilter.getBitSize() = " + bloomFilter.getBitSize());
+    System.out.println("hashedIndexes = " + hashedIndexes);
+
+    bloomFilterRedisHandler.init(bloomFilter);
+
+    long start = System.nanoTime();
+    bloomFilterRedisHandler.add(bloomFilter, "value");
+    long millis = Duration.ofNanos(System.nanoTime() - start).toMillis();
+    System.out.println("millis = " + millis);
+  }
+
+}
